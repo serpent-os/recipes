b4fae91b1e9866ac677fb485a1f3b76db51d73ac
Peter O'Connor <sunnyflunk@serpentos.com>
2022-09-14 18:34:37 +1000

    devel/boulder: Update to latest git build
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- devel/boulder/update_sources.sh
+++ devel/boulder/update_sources.sh
@@ -1,13 +1,18 @@
 pu () {
-    curl -s -L https://gitlab.com/serpent-os/core/$1/-/archive/main/$1-main.tar.gz -o /tmp/tmp-dwnld
+    curl -s -L https://github.com/serpent-os/$1/archive/refs/heads/$2.tar.gz -o /tmp/tmp-dwnld
     SUM=$(sha256sum /tmp/tmp-dwnld | cut -f1 -d' ')
-    echo "    - https://gitlab.com/serpent-os/core/$1/-/archive/main/$1-main.tar.gz:
+    echo "    - https://github.com/serpent-os/$1/archive/refs/heads/$2.tar.gz:
         hash: ${SUM}
+        rename: ${1}.tar.gz
         stripdirs: 1
-        unpackdir: \"$1/\""
+        unpackdir: \"$3/\""
 }
 
 for i in boulder moss-config moss-core moss-deps moss-fetcher moss-format moss-vendor; do
-    pu ${i}
+    pu ${i} main ${i}
     rm /tmp/tmp-dwnld
 done
+
+pu elf-d dynamiclinking moss-vendor/vendor/elf/elf-d; rm /tmp/tmp-dwnld
+pu lmdb-d main moss-vendor/subprojects/lmdb-d; rm /tmp/tmp-dwnld
+pu xxhash-d main moss-vendor/subprojects/xxhash-d; rm /tmp/tmp-dwnld
