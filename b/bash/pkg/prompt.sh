50618c057b1b1b143772a449818fc48fff79e48d
Ikey Doherty <ikey@serpentos.com>
2024-08-01 02:06:06 +0000

    bash: Refix
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- b/bash/pkg/prompt.sh
+++ b/bash/pkg/prompt.sh
@@ -1,6 +1,4 @@
-# Begin /usr/share/defaults/etc/bashrc
-
-alias ls="ls --color=auto -F"
+# Begin /usr/share/defaults/etc/profile.d/prompt.sh
 
 e="\$"
 if [ "$UID" = "0" ]; then
@@ -10,12 +8,4 @@ fi
 PS1="\[\033[01;35m\]\u@\h\[\033[00m\] $e \[\033[01;34m\]\w\[\033[00m\] "
 unset e
 
-if [ -e /usr/share/defaults/etc/profile ] ; then
-    source /usr/share/defaults/etc/profile
-fi
-
-if [ -e /etc/bashrc ] ; then
-    source /etc/bashrc
-fi
-
-# End /usr/share/defaults/etc/bash.bashrc
+# End /usr/share/defaults/etc/profile.d/prompt.sh
