79f4bea2402be4d6ee65661d116c31356f607e06
Ikey Doherty <ikey@serpentos.com>
2024-08-01 01:57:54 +0000

    bash: Share prompt between profile + interactive
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- b/bash/pkg/prompt.sh
+++ b/bash/pkg/prompt.sh
@@ -2,6 +2,14 @@
 
 alias ls="ls --color=auto -F"
 
+e="\$"
+if [ "$UID" = "0" ]; then
+    e="#"
+fi
+
+PS1="\[\033[01;35m\]\u@\h\[\033[00m\] $e \[\033[01;34m\]\w\[\033[00m\] "
+unset e
+
 if [ -e /usr/share/defaults/etc/profile ] ; then
     source /usr/share/defaults/etc/profile
 fi
