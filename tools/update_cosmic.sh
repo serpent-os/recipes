a7d3d2acead230e8c5574e94ecb6731adf2553d1
Ikey Doherty <ikey@serpentos.com>
2024-11-17 09:35:30 +0000

    tools: Nasty ass script to update cosmic to latest gits
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ tools/update_cosmic.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+set -x
+
+REPO="pop-os"
+
+# The version of the cosmic desktop
+VERSION="1.0.0-alpha.3"
+
+# Find all `stone.yaml` files that contain pop-os in the upstreams section
+# and update the upstream to the latest commit
+
+for recipe in $(find . -name "stone.yaml"); do
+  # Get the upstreams section
+  upstreams=$(yq ".upstreams" $recipe)
+
+  # Check if the upstreams section contains pop-os
+  if [[ $upstreams == *"pop-os"* ]]; then
+    # Get the package name
+    package_name=$(yq ".name" $recipe)
+
+    if [[ $package_name == "cosmic-workspaces" ]]; then
+      package_name="cosmic-workspaces-epoch"
+    elif [[ $package_name == "pop-os-launcher" ]]; then
+      continue
+    elif [[ $package_name == "pop-os-icon-theme" ]]; then
+      continue
+    elif [[ $package_name == "system76-power" ]]; then
+      continue
+    fi
+
+    # Get the latest commit from the github API
+    latest_commit=$(curl -s "https://api.github.com/repos/$REPO/$package_name/commits" | jq -r '.[0].sha')
+
+    # change to working directory
+    pushd $(dirname $recipe)
+
+    # Update the recipe
+    boulder recipe update --ver $VERSION --upstream "git|$latest_commit" --overwrite stone.yaml
+
+    popd
+  fi
+done
