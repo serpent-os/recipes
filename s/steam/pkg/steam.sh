1fe4c55c2fb2ed3cd5a6f7fe870c4d08b6d16b0b
Ikey Doherty <ikey@serpentos.com>
2024-12-18 11:48:48 +0000

    steam: Round 2 of fixing env vars for desktop launch
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- s/steam/pkg/steam.sh
+++ s/steam/pkg/steam.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [[ ! -z "${SERPENT_STEAM_SHIM}" ]]; then
-    exec /usr/lib/steam/bin_steam.sh "$@"
-fi
+case $LD_PRELOAD in
+    *libstdc++.so.6*)
+        ;;
+    "")
+        export LD_PRELOAD="libstdc++.so.6"
+        ;;
+    *)
+        export LD_PRELOAD="${LD_PRELOAD}:libstdc++.so.6"
+        ;;
+esac
 
-export SERPENT_STEAM_SHIM=1
-if [[ ! -z "${LD_PRELOAD}" ]]; then
-    export LD_PRELOAD="${LD_PRELOAD}:libstdc++.so.6"
-else
-    export LD_PRELOAD="libstdc++.so.6"
-fi
-
-exec /usr/lib/steam/bin_steam.sh "$@"
+exec /usr/lib/steam/steam "$@"
