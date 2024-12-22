8a27be9785dc780abb6352d4f2f8da323f60ec1a
Ikey Doherty <ikey@serpentos.com>
2024-12-22 17:47:15 +0000

    moss: Improve `command-not-found` functionality
     - Default to N not Y
     - Support `sysbinary` fallback search
     - Make sure we exit when folks just whack "enter"
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- m/moss/pkg/80-command-not-found.sh
+++ m/moss/pkg/80-command-not-found.sh
@@ -2,19 +2,21 @@ command_not_found_handle() {
     local pkgs cmd=$1
     local FUNCNEST=10
 
-    pkg=`NO_COLOR=1 moss info binary\($cmd\) 2>&1`
-    if [[ $? -eq 0 ]]; then
-    	pkg=$(echo "$pkg" | head -n1 | awk {'print $2'})
-    	local r
-    	read -r -p "Install package '$pkg'? [Y/n] " r
-    	if [[ $r == Y || $r == y ]]; then
-    		echo "sudo moss install 'binary($cmd)'"
-    		sudo moss install "$pkg"
-    		return
-    	else
-    		return 0
-    	fi
-    fi
+    for bin_type in binary sysbinary ; do
+        pkg=`NO_COLOR=1 moss info $bin_type\($cmd\) 2>&1`
+        if [[ $? -eq 0 ]]; then
+            pkg=$(echo "$pkg" | head -n1 | awk {'print $2'})
+            local r
+            read -r -p "Install package '$pkg'? [y/N] " r
+            case $r in
+                [Yy]* ) ;;
+                * ) return 0;;
+            esac
+            echo " >> sudo moss install '$bin_type($cmd)'"
+            sudo moss install "$pkg"
+            return
+        fi
+    done
 
     printf "bash: %s: command not found\n" $cmd >&2
 
